3845a9a8a0747e6873ec9a58b56e902508308941
ilimaylla <maria.limaylla@gmail.com>
2025-05-09 12:07:41 +0200

    Changes for import

--- js/src/FeatureModel.js
+++ js/src/FeatureModel.js
@@ -40,6 +40,66 @@ export default class FeatureModel {
     this.featureModel = tree;
   }
 
+  getImports(deep=false){
+    let paths = [];
+    const protocols = ['git', 'npm', 'file'];              
+    const tree = this.getFeatureModel();
+    const namespace = this.getNamespace(tree);
+    const listImports = tree.imports();
+
+    if (listImports !== null) {          
+      listImports.children.forEach((childImport) => {
+        if (childImport instanceof UVLJavaScriptParser.ImportLineContext) {
+          childImport.children.forEach((childImportLine) => {
+            let nameChildImport = childImportLine.ns.getText().trim();
+            let aliasChildImport = this.getAliasImport(childImportLine);
+            let nameProtocol = null;
+            let importType = null;
+            if (childImportLine instanceof UVLJavaScriptParser.ImportLineExtendedContext) {
+              nameProtocol = childImportLine.pp.getText().trim();
+              for (const protocol of protocols) {
+                if (nameProtocol.startsWith(protocol)) {
+                  importType = protocol;
+                  nameProtocol = nameProtocol.slice(protocol.length+1);
+                  break;
+                }
+              }
+            }
+            const importDetail = {
+                  "namespace": namespace,
+                  "name": nameChildImport,
+                  "importType": importType,
+                  "importPath": nameProtocol,
+                  "alias": aliasChildImport
+              };
+            paths.push(importDetail);
+          });
+        }
+      });
+    }          
+    return paths;
+  }
+
+  getNamespace(tree) {
+    const namespace = tree.namespace();
+    let name = null;
+    if (namespace instanceof UVLJavaScriptParser.NamespaceContext) {
+        namespace.children.forEach((child) => {
+          if (child instanceof UVLJavaScriptParser.ReferenceContext) {
+              name = child.getText();
+              return true;
+          }
+        });
+    }
+    return name;
+  }
+
+  getAliasImport(childImport){
+    if (childImport.alias!== null){
+      return childImport.alias.getText().trim()
+    }
+  }
+
   getFeatureModel() {
     return this.featureModel;
   }
--- uvl/UVLJavaScript.g4
+++ uvl/UVLJavaScript.g4
@@ -12,6 +12,24 @@ import UVLBase;
   let lastToken = null;
 }
 
+importLine: importLineBasic | importLineExtended;
+importLineBasic: ns=reference ('as' alias=reference)? NEWLINE;
+importLineExtended: ns=reference 'from' pp=protocolPath ('as' alias=reference)? NEWLINE;
+
+protocolPath: route | GIT | npm;
+
+route: 'file:' (ABS_PATH | REL_PATH);
+npm: 'npm:' NPM (VERSION_NUMBER | ID_STRICT);
+
+ABS_PATH: (QUOTE)? [a-zA-Z] ':'  (SLASH [a-zA-Z0-9_.-]+)+ (QUOTE)?;
+REL_PATH: (QUOTE)? ('.' | '..')? (SLASH [a-zA-Z0-9_.-]+)+ (QUOTE)?;
+GIT: 'git:' [:/a-zA-Z0-9_.-]+ '.git' ('#' [a-zA-Z0-9_.-]+)?;
+NPM: [a-zA-Z0-9_.-]+ ':';
+VERSION_NUMBER: ('^' | '~')? [0-9]+ ('.' [0-9]+)* ;
+
+QUOTE: '"' | '\'';
+SLASH: '\\' | DIV;
+
 OPEN_PAREN : '(' {this.opened += 1;};
 CLOSE_PAREN : ')' {this.opened -= 1;};
 OPEN_BRACK : '[' {this.opened += 1;};
